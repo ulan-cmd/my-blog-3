14b47cbb3eca3816d9dd54aa67f868767f1be378
itjake <tokonov.tagai@gmail.com>
2022-07-29 09:24:57 +0600

    DASFFF

---
+++ src/components/Iphone/Iphone.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from "./Iphone.module.css";
+
+const Iphone = () => {
+    const title = {
+      textAlign: "center"
+    }
+
+    return (
+      <div>
+        <h1 style={title}>Здесь будут айфоны</h1>
+      </div>
+    );
+  };
+
+  export default Iphone;
---
+++ src/components/MacBook/MacBook.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from "./MacBook.module.css";
+
+const MacBook = () => {
+  const title =
+    {
+      textAlign: "center"
+    }
+
+  return (
+    <div>
+      <h1 style={title}>Здесь будут МакБуки</h1>
+    </div>
+  );
+};
+
+export default MacBook;
